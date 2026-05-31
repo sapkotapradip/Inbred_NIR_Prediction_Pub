b43fd1b9b54d53796de9666917c903680eaa6f45
Sapkotapradip <prdp@tamu.edu>
2026-05-31 16:01:23 -0500

    add

---
+++ codes/genetic_gain.R
@@ -0,0 +1,5 @@
+## Using methods from Wang et al., 2025 The Plant Phenome ####################################################
+## Codes for assessing breeder's equation ####################################################################
+
+
+
